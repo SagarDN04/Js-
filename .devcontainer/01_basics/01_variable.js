872f160d95e66d4ce8ddf89d0209b7a15b5c4dc2
Sagar D Netalkar <sagarnetalkar55@gmail.com>
2026-04-25 13:26:42 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="tryerror01@gmail.com"
+var accountPassword="12345"
+accountCity = "jaipur"
+ 
+accoutEmail="how@gmail.com"
+accountPassword = "212121"
+accountCity="Kolhapur"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*Prefer not to use var due to issue in block scope and functional scope */
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";//treat all JS code as newer version
+//code redablity should be high
+console.log(3+3) ;
+console.log("sagar")
+
+//primitive data types
+let name="sagar" //string=>""
+let age=21 //int 2 to power 53
+let isLogged=false //boolean =true/false
+//bigint
+//null=>standalone value
+//undefined=>
+//symbol  for uniqueness
+
+//objects
+console.log(typeof undedfined);
+console.log(typeof null);
