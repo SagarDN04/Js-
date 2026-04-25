71ec04be26e6902565d99a04cba941fc2ac58527
Sagar D Netalkar <sagarnetalkar55@gmail.com>
2026-04-25 14:54:38 +0000

    conversion of data types

--- .devcontainer/01_basics/03_conversionop.js
+++ .devcontainer/01_basics/03_conversionop.js
@@ -16,3 +16,50 @@ console.log(valueInNumber)
     null => 0
     undefined => NaN
 */
+
+
+
+//********** Operations ***********//
+
+let value =3
+let negValue = -value
+console.log(negValue)
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**3);
+console.log(2%2);
+
+
+let strl1="hello"
+let strl2="bro"
+let strl3=strl1+strl2
+console.log(strl3)
+
+/*console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+"2");
+console.log("1"+2+2);    
+
+console.log((3+4)*5%3)*/
+
+console.log(+true);
+//console.log(true+);
+console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num1)
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
