3f18d3495e1fd34dc665f6c6298f487312db3ce8
Sagar D Netalkar <sagarnetalkar55@gmail.com>
2026-04-25 15:05:35 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0); // in this instead of comparison nulll is assigned to 0
+console.log(null<=0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+//=== strict check 
+console.log("2"===2); // it checks shring equal to int
