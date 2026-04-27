4c08c534fd6e4c53f2b2a1fbb844f5fcef3a150e
Sagar D Netalkar <sagarnetalkar55@gmail.com>
2026-04-27 10:18:39 +0000

    nums

---
+++ .devcontainer/01_basics/06_numsandmath.js
@@ -0,0 +1,14 @@
+const score = 400
+console.log(score)
+const balance = new Number(100)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+
+const demoNum=1123.9768
+console.log(demoNum.toPrecision(4))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))    //by default it given USA number grading 
+
+//++++++++++++++++++ Maths ++++++++++++++++++//
